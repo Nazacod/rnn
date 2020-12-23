20daec7aca7b898cc45bf02649c14ca9b2d49800
Nazacod <s02190158@gse.cs.msu.ru>
2020-12-23 22:33:55 +0300

    create project

--- lm.py
+++ lm.py
@@ -229,8 +229,8 @@ def run_epoch(lr, model, data, word_to_id, loss_fn, optimizer=None, device=None,
 def get_small_config():
     config = {'lr': 0.1, 'lr_decay': 0.5,
               'max_grad_norm': 5, 'emb_size': 200,
-              'hidden_size': 200, 'max_epoch': 5,
-              'max_max_epoch': 13, 'batch_size': 64,
+              'hidden_size': 200, 'max_epoch': 90,
+              'max_max_epoch': 100, 'batch_size': 64,
               'num_steps': 35, 'num_layers': 2,
               'vocab_size': 10000}
     return config
