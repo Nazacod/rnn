bc2fedfd11697fd405e094323001c0c8753f3249
Nazacod <s02190158@gse.cs.msu.ru>
2021-04-17 01:16:30 +0300

    nice

--- lm.py
+++ lm.py
@@ -12,12 +12,12 @@ EOS_TOKEN = '<eos>'
 device_glob = ""
 np.random.seed(42)
 
-config = {'lr': 0.01, 'lr_decay': 0.83,
+config = {'lr': 0.01, 'lr_decay': 0.9,
           'max_grad_norm': 5, 'emb_size': 256,
-          'hidden_size': 1024, 'max_epoch': 6,
-          'max_max_epoch': 39, 'batch_size': 20,
+          'hidden_size': 256, 'max_epoch': 6,
+          'max_max_epoch': 13, 'batch_size': 40,
           'num_steps': 35, 'vocab_size': 10000,
-          'dropout_rate': 1}
+          'dropout_rate': 0.8}
 
 # config = {'lr': 0.01, 'lr_decay': 0.9,
 #           'max_grad_norm': 5, 'emb_size': 256,
