039bf0bc108131b332e6872ea481489eaf149091
Nazacod <s02190158@gse.cs.msu.ru>
2021-04-17 13:00:15 +0300

    nice

--- lm.py
+++ lm.py
@@ -13,8 +13,8 @@ np.random.seed(42)
 
 config = {'lr': 0.01, 'lr_decay': 0.9,
           'max_grad_norm': 5, 'emb_size': 256,
-          'hidden_size': 256, 'max_epoch': 6,
-          'max_max_epoch': 13, 'batch_size': 64,
+          'hidden_size': 256, 'max_epoch': 9,
+          'max_max_epoch': 30, 'batch_size': 64,
           'num_steps': 100, 'vocab_size': 10000,
           'dropout_rate': 0.8}
 
