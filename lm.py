f72e19aa482bcd7853da6c1709886bd921209308
Nazacod <s02190158@gse.cs.msu.ru>
2021-04-18 01:31:52 +0300

    nice

--- lm.py
+++ lm.py
@@ -12,11 +12,11 @@ EOS_TOKEN = '<eos>'
 np.random.seed(42)
 
 config = {'lr': 0.08, 'lr_decay': 0.9,
-          'max_grad_norm': 5, 'emb_size': 1024,
-          'hidden_size': 1024, 'max_epoch': 10,
-          'max_max_epoch': 50, 'batch_size': 64,
-          'num_steps': 35, 'vocab_size': 10000,
-          'dropout_rate': 0.4}
+          'max_grad_norm': 5, 'emb_size': 512,
+          'hidden_size': 512, 'max_epoch': 6,
+          'max_max_epoch': 40, 'batch_size': 64,
+          'num_steps': 65, 'vocab_size': 10000,
+          'dropout_rate': 1.0}
 
 # config = {'lr': 0.01, 'lr_decay': 0.9,
 #           'max_grad_norm': 5, 'emb_size': 256,
