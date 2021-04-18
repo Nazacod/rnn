8b1e5e48d629e310d68ad0546a8449fb0d4cb68e
Nazacod <s02190158@gse.cs.msu.ru>
2021-04-18 17:34:45 +0300

    nice

--- lm.py
+++ lm.py
@@ -16,7 +16,7 @@ config = {'lr': 0.0075, 'lr_decay': 0.9,
           'hidden_size': 256, 'max_epoch': 9,
           'max_max_epoch': 30, 'batch_size': 64,
           'num_steps': 35, 'vocab_size': 10000,
-          'dropout_rate': 1.0}
+          'dropout_rate': 0.9}
 
 # config = {'lr': 0.01, 'lr_decay': 0.9,
 #           'max_grad_norm': 5, 'emb_size': 256,
